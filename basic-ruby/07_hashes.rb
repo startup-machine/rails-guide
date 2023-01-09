a0c0bb747e5030635e237508f7b06bce1f8dc872
cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2023-01-09 18:53:22 -0300

    concatenacao e interpolacao

---
+++ basic-ruby/07_hashes.rb
@@ -0,0 +1,7 @@
+# um array que você determina o índice
+
+h = {"a" => "jackson", "r" => "rails"}
+
+puts h["r"]
+
+puts h["a"]
---
+++ basic-ruby/08_strings.rb
@@ -0,0 +1,19 @@
+x = "cleidson"
+y = "medeiros"
+
+puts x
+puts x.class
+puts y
+puts y.class
+
+
+puts x << y # a shovel modifica o x
+puts x + y 
+
+puts "========================================="
+
+#interpolação de variáveis
+
+nome = "Cleidson Medeiros"
+
+puts "#{nome} Bem vindo!!"

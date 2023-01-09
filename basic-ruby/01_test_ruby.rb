a7f459698f043262e9975f2903c6a565ae2a160d
cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2023-01-09 17:00:13 -0300

    implement get and chomp

--- basic-ruby/01_test_ruby.rb
+++ basic-ruby/01_test_ruby.rb
@@ -3,5 +3,6 @@ puts "Hello World!!"
 puts "Digite seu nome"
 
 nome = gets.chomp # chomp tira a quebra de linha
+                  # o gets ler o que o usuario digitar                   
 
 puts "hello " + nome
